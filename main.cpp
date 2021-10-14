c3054288ef56f7707bfcdac45c181c7ebbc72e6f
1619413 <1619413@students.wits.ac.za>
2021-10-14 10:19:04 +0200

    almost working board output

--- main.cpp
+++ main.cpp
@@ -4,17 +4,33 @@
 #include<iterator>
 #include<queue>
 #include<set>
-#include <sstream>
-#include <map>
+#include<algorithm>
+#include<sstream>
+#include<map>
 
 using namespace std;
 
 //For outputting in submission one
-vector<vector<string>>vecBoardOutput(17);
+vector<vector<string>>vecBoardOutput(16);
 //First vector split by spaces
 vector<string>vecLine;
 //Second vector splitting by / for the state of the board
 vector<string>vecBoardLine;
+
+vector<string>vecLines;
+
+vector<string>vecColStr={"a","b","c","d","e","f","g"};
+map<string,int>animalToOut{
+    {"P",0},{"p",1},{"S",2},{"s",3},{"G",4},{"g",5},{"M",6},{"m",7},
+    {"E",8},{"e",9},{"L",10},{"l",11},{"C",12},{"c",13},{"Z",14},{"z",15}
+};
+
+map<int,string>lineToOut{
+    {0,"white pawn: "},{1,"black pawn: "},{2,"white superpawn: "},{3,"black superpawn: "},{4,"white giraffe: "},{5,"black giraffe: "},
+    {6,"white monkey: "},{7,"black monkey: "},{8,"white elephant: "},{9,"black elephant: "},{10,"white lion: "},{11,"black lion: "},
+    {12,"white crocodile: "},{13,"black crocodile: "},{14,"white zebra: "},{15,"black zebra: "}
+};
+
 
 //s=string to split, del=thing to split by in string form, vec=1D vector to store the split in
 void stringTokenizer(string s, string del, vector<string>&vec)
@@ -31,30 +47,76 @@ void stringTokenizer(string s, string del, vector<string>&vec)
     
 }
 
-
-int main(){
+void printBoard(){
     string line;
-    getline(cin,line);
-    stringTokenizer(line," ",vecLine);
-    stringTokenizer(vecLine[0],"/",vecBoardLine);
-    vector<string>vecColStr={"a","b","c","d","e","f"};
-    map<string,int>animalToOut{
-        {"P",0},{"p",1},{"S",2},{"s",3},{"G",4},{"g",5},{"M",6},{"m",7},
-        {"E",8},{"e",9},{"L",10},{"l",11},{"C",12},{"c",13},{"Z",14},{"z",15}
-    };
+    for(int numInput=0;numInput<vecLines.size();numInput++){
+        vecBoardLine.clear();
+        vecLine.clear();
+        line=vecLines[numInput];
+        stringTokenizer(line," ",vecLine);
+        stringTokenizer(vecLine[0],"/",vecBoardLine);
+        reverse(vecBoardLine.begin(),vecBoardLine.end());
+    
 
-    // for(auto x:vecBoardLine){
-    //     cout<<x<<endl;
-    // }
+        int skip,pos;
 
-    // for(auto x:vecLine){
-    //     cout<<x<<endl;
-    // }
+        for(int row=6;row>=0;row--){
+            for(int col=0;col<7;col++){
+                // cout<<vecBoardLine[row].substr(col,1)<<endl;
+                if(isdigit(vecBoardLine[row][col])){
+                    skip=stoi(vecBoardLine[row].substr(col,1));
+                    col+=skip-1;
+                }else{
+                    pos=animalToOut[vecBoardLine[row].substr(col,1)];
+                    vecBoardOutput[pos].push_back(vecColStr[col]+to_string(row+1));
+                }
+            }
+        }
 
-    for(int row=7;row>=0;row--){
-        for(int col=0;col<8;col++){
+        for(int i=0;i<16;i++){
+            line="";
+            line+=lineToOut[i];
+            int elementNum=0;
+            elementNum=vecBoardOutput[i].size();
+            for(int j=0;j<elementNum;j++){
+                line+=vecBoardOutput[i][j]+" ";
+            }
+            cout<<line<<endl;
+        }
 
+        if(vecLine[1]=="b"){
+            cout<<"side to play: black\n";
+        }else{
+            cout<<"side to play: white\n";
+        }
+
+        cout<<endl;
+
+        for(int i=0;i<17;i++){
+            vecBoardOutput[i].clear();
         }
     }
+    
+}
+
+
+int main(){
+    string numInput;
+    string line;
+
+    getline(cin,numInput);
+    vecLines.resize(stoi(numInput));
+    for(int k=0;k<stoi(numInput);k++){
+        getline(cin,line);
+        vecLines[k]=line;
+    }
+
+    printBoard();
+
+    
+
+        
+
+    
 
 }
