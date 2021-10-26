b4018187e8a394fc062a8908bdf5f07c8058ffb6
1619413 <1619412@students.wits.ac.za>
2021-10-26 20:33:52 +0200

    starting on sub 2

--- main.cpp
+++ main.cpp
@@ -12,11 +12,13 @@ using namespace std;
 
 //For outputting in submission one
 vector<vector<string>>vecBoardOutput(16);
+//For tracking the board state
+vector<vector<string>>vecBoardState(7,vector<string>(7,"-"));
 //First vector split by spaces
 vector<string>vecLine;
 //Second vector splitting by / for the state of the board
 vector<string>vecBoardLine;
-
+//Stores all the orginal lines entered 
 vector<string>vecLines;
 
 vector<string>vecColStr={"a","b","c","d","e","f","g"};
@@ -31,9 +33,8 @@ map<int,string>lineToOut{
     {12,"white crocodile: "},{13,"black crocodile: "},{14,"white zebra: "},{15,"black zebra: "}
 };
 
-
 //s=string to split, del=thing to split by in string form, vec=1D vector to store the split in
-void stringTokenizer(string s, string del, vector<string>&vec)
+void stringTokenizer(std::string s, std::string del, std::vector<std::string>&vec)
 {
     int start = 0;
     int end = s.find(del);
@@ -47,9 +48,9 @@ void stringTokenizer(string s, string del, vector<string>&vec)
     
 }
 
-void insertionSort(vector<string>&array) {
+void insertionSort(std::vector<std::string>&array) {
    int j;
-   string key;
+   std::string key;
    int size=array.size();
    for(int i = 1; i<size; i++) {
       key = array[i];//take value
@@ -64,44 +65,63 @@ void insertionSort(vector<string>&array) {
 
 void printBoard(){
     string line;
+    //Loop through all board lines entered
     for(int numInput=0;numInput<vecLines.size();numInput++){
+        //Clear splitting vectors
         vecBoardLine.clear();
         vecLine.clear();
+        //Get the line to split
         line=vecLines[numInput];
+        //Split by spaces to give board, move number and colour to play
         stringTokenizer(line," ",vecLine);
+        //Split by slashes to give each row for the board ordered in descending row order
         stringTokenizer(vecLine[0],"/",vecBoardLine);
+        //Reverse the vector holding the board line to give the board rows in ascending order
         reverse(vecBoardLine.begin(),vecBoardLine.end());
     
 
         int skip,pos;
         int col,colToRead;
-        for(int row=6;row>=0;row--){//FIXME: col should no correspond to the actual col we read from vecBoardLine 
+        for(int row=6;row>=0;row--){
             col=0;
+            //Loop through each letter in the string for a board line
             for(int colToRead=0;colToRead<vecBoardLine[row].size();colToRead++){
-
+                //Check for a skip digit
                 if(isdigit(vecBoardLine[row][colToRead])){
+                    //Get the number in the correct form
                     skip=stoi(vecBoardLine[row].substr(colToRead,1));
+                    //Perform the column skip
                     col+=skip;
                 }else{
+                    //Get the value of the row in the vector that will output to console
                     pos=animalToOut[vecBoardLine[row].substr(colToRead,1)];
+                    //Push the position of the piece to the correct row in the vector that will output to console
                     vecBoardOutput[pos].push_back(vecColStr[col]+to_string(row+1));
+                    //Increment the column number normally
                     col+=1;
                 }
             }
         }
 
+        //Go through all the rows in the vector that will output to console
         for(int i=0;i<16;i++){
             line="";
+            //Start the line with the initial required string
             line+=lineToOut[i];
             int elementNum=0;
+            //Get the length of the vector line containing the piece positions
             elementNum=vecBoardOutput[i].size();
+            //Sort the vector line of piece positions in ascending order
             insertionSort(vecBoardOutput[i]);
+            //Add all the positions to the line
             for(int j=0;j<elementNum;j++){
                 line+=vecBoardOutput[i][j]+" ";
             }
+            //Output the line in console
             cout<<line<<endl;
         }
 
+        //Output the final line of whose turn it is
         if(vecLine[1]=="b"){
             cout<<"side to play: black\n";
         }else{
@@ -110,6 +130,7 @@ void printBoard(){
 
         cout<<endl;
 
+        //CLear the vector containing the piece positions
         for(int i=0;i<16;i++){
             vecBoardOutput[i].clear();
         }
@@ -117,7 +138,52 @@ void printBoard(){
     
 }
 
+void initBoard(){
+    //Loop through all board lines entered
+    for(int numInput=0;numInput<vecLines.size();numInput++){
+        //Clear splitting vectors
+        vecBoardLine.clear();
+        vecLine.clear();
+        //Get the line to split
+        line=vecLines[numInput];
+        //Split by spaces to give board, move number and colour to play
+        stringTokenizer(line," ",vecLine);
+        //Split by slashes to give each row for the board ordered in descending row order
+        stringTokenizer(vecLine[0],"/",vecBoardLine);
+        //Reverse the vector holding the board line to give the board rows in ascending order
+        reverse(vecBoardLine.begin(),vecBoardLine.end());
+    
 
+        int skip,pos;
+        int col,colToRead;
+        for(int row=6;row>=0;row--){
+            col=0;
+            //Loop through each letter in the string for a board line
+            for(int colToRead=0;colToRead<vecBoardLine[row].size();colToRead++){
+                //Check for a skip digit
+                if(isdigit(vecBoardLine[row][colToRead])){
+                    //Get the number in the correct form
+                    skip=stoi(vecBoardLine[row].substr(colToRead,1));
+                    //Perform the column skip
+                    col+=skip;
+                }else{
+                    //Get the value of the row in the vector that will output to console
+                    pos=animalToOut[vecBoardLine[row].substr(colToRead,1)];
+                    //Push the position of the piece to the correct row in the vector that will output to console
+                    vecBoardOutput[pos].push_back(vecColStr[col]+to_string(row+1));
+                    //Increment the column number normally
+                    col+=1;
+                }
+            }
+        }
+    }
+
+}
+void movesLion(){
+    //TODO: Implement a better board state vector
+    //TODO: Check the available moves for the lion and output them
+
+}
 
 
 int main(){
@@ -131,7 +197,7 @@ int main(){
         vecLines[k]=line;
     }
 
-    printBoard();
+    // printBoard();
 
     
 
